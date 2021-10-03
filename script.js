cd6ea92cc3857bdb0d684ee2737e68a48485ca63
NikitaMishraaa <nikitarm1997@gmail.com>
2021-10-03 20:55:05 +0530

    changed

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const tipCalcy = () => {
     let perc = document.getElementById('tip_perc').value ;
 
     let tip = amount * (perc/100);
-    alert(tip)
+    // alert(tip)
     let total = tip + Number(amount);
-    alert(total)
+    // alert(total)
 
     document.getElementById('tip_amount').value = tip;
     document.getElementById('total_tip').value = total;
